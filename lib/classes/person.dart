0ab4be0c1c85669d2aef0b55675ab5435c5301cc
Leila Moraes <leilamoraesdev@gmail.com>
2023-10-05 17:01:07 -0300

    feat: class Person get, set and constructor

--- lib/classes/person.dart
+++ lib/classes/person.dart
@@ -1,1 +1,35 @@
+class Person {
+  String _name = "";
+  double _weight = 0;
+  double _height = 0;
 
+  Person(String name, double weight, double height) {
+    _name = name;
+    _weight = weight;
+    _height = height;
+  }
+
+  void setName(String name) {
+    _name = name;
+  }
+
+  void setWeight(double weight) {
+    _weight = weight;
+  }
+
+  void setHeight(double height) {
+    _height = height;
+  }
+
+  String getName() {
+    return _name;
+  }
+
+  double getWeight() {
+    return _weight;
+  }
+
+  double getHeight() {
+    return _height;
+  }
+}
